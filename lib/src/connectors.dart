c19a4c43ea5ede864f40ac538059dff3d7bc04e3
Chulwoo Park <park@chulwoo.dev>
2020-11-18 15:05:45 +0900

    Add ConnectorTheme, ThemedConnectorComponent and some properties to SolidLineConnector

--- lib/src/connectors.dart
+++ lib/src/connectors.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+import 'connector_theme.dart';
+import 'timeline.dart';
 import 'timeline_theme.dart';
 
 enum ConnectorStyle {
@@ -8,24 +10,65 @@ enum ConnectorStyle {
   transparent,
 }
 
-// TODO
-class SolidLineConnector extends StatelessWidget {
+/// A thin line, with padding on either side.
+///
+/// The box's total cross axis size(width or height, depend on [direction]) is controlled by [space].
+///
+/// The appropriate padding is automatically computed from the cross axis size.
+class SolidLineConnector extends StatelessWidget with ThemedConnectorComponent {
+  /// The axis along which the timeline scrolls.
+  ///
+  /// If this is null, then the [TimelineThemeData.direction] is used.
   final Axis direction;
+
+  /// The connector's cross axis size extent.
+  ///
+  /// The connector itself is always drawn as a line that is centered within the size specified by this value.
+  ///
+  /// If this is null, then the [DividerThemeData.space] is used. If that is also null, then this defaults to
+  /// double.infinity.
+  final double space;
+
+  /// The thickness of the line drawn within the connector.
+  ///
+  /// If this is null, then the [ConnectorThemeData.thickness] is used which defaults to 2.0.
   final double thickness;
+
+  /// The color to use when painting the line.
+  ///
+  /// If this is null, then the [ConnectorThemeData.color] is used. If that is also null, then [ThemeData.color] is
+  /// used.
   final Color color;
+
+  /// The amount of empty space to the leading edge of the connector.
+  ///
+  /// If this is null, then the [ConnectorThemeData.indent] is used. If that is also null, then this defaults to 0.0.
+  final double indent;
+
+  /// The amount of empty space to the trailing edge of the connector.
+  ///
+  /// If this is null, then the [ConnectorThemeData.endIndent] is used. If that is also null, then this defaults to 0.0.
+  final double endIndent;
 
   const SolidLineConnector({
     Key key,
     this.direction,
     this.thickness,
+    this.space,
+    this.indent,
+    this.endIndent,
     this.color,
-  }) : super(key: key);
-
+  })  : assert(thickness == null || thickness >= 0.0),
+        assert(space == null || space >= 0.0),
+        assert(indent == null || indent >= 0.0),
+        assert(endIndent == null || endIndent >= 0.0),
+        super(key: key);
   @override
   Widget build(BuildContext context) {
-    final direction = this.direction ?? TimelineTheme.of(context).direction;
-    final thickness = this.thickness ?? 2.0;
-    final color = this.color ?? Colors.blue;
+    // TODO: apply other properties
+    final direction = getEffectiveDirection(context);
+    final thickness = getEffectiveThickness(context);
+    final color = getEffectiveColor(context);
     switch (direction) {
       case Axis.vertical:
         return Container(
@@ -43,11 +86,12 @@ class SolidLineConnector extends StatelessWidget {
   }
 }
 
+/// A transparent connector for start, end [TimelineNode] of the [Timeline].
+///
+/// This connector will be not displayed, it only occupies an area.
 class TransparentConnector extends StatelessWidget {
   const TransparentConnector();
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  Widget build(BuildContext context) => Container();
 }
