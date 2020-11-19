19cd0f86a7a4e953791d7bc4b755cffb384eef0f
Chulwoo Park <park@chulwoo.dev>
2020-11-19 09:35:14 +0900

    Update export list

--- lib/timelines.dart
+++ lib/timelines.dart
@@ -1,8 +1,9 @@
 library timelines;
 
-export 'package:timelines/src/timeline_node.dart';
-export 'package:timelines/src/indicators.dart';
+export 'package:timelines/src/connector_theme.dart';
 export 'package:timelines/src/connectors.dart';
 export 'package:timelines/src/indicator_theme.dart';
+export 'package:timelines/src/indicators.dart';
+export 'package:timelines/src/timeline_node.dart';
 export 'package:timelines/src/timeline_theme.dart';
 export 'package:timelines/src/timeline_tile.dart';
