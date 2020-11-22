3d5bf5db0c83543c40ca480d3814af2911ca85c6
Chulwoo Park <park@chulwoo.dev>
2020-11-22 18:18:15 +0900

    Modify example to use connected constructor

--- example/lib/showcase/package_delevery_tracking.dart
+++ example/lib/showcase/package_delevery_tracking.dart
@@ -10,156 +10,189 @@ class PackageDeliveryTrackingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TitleAppBar('Package Delivery Tracking'),
-      body: TimelineTheme(
-        data: TimelineThemeData(
-          color: Color(0xff989898),
-          indicatorTheme: IndicatorThemeData(
-            position: 0,
-            size: 20.0,
-          ),
-          connectorTheme: ConnectorThemeData(
-            thickness: 2.5,
-          ),
-        ),
-        child: Center(
-          child: Container(
-            width: 360.0,
-            child: Card(
-              margin: EdgeInsets.all(20.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Row(
-                      children: [
-                        Text(
-                          'Delivery #4565',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Spacer(),
-                        Text(
-                          '25/12/2020',
-                          style: TextStyle(
-                            color: Color(0xffb6b2b2),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Divider(height: 1.0),
-                  DefaultTextStyle(
-                    style: TextStyle(
-                      color: Color(0xff9b9b9b),
-                      fontSize: 12.5,
-                    ),
-                    child: Padding(
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final data = _data(index + 1);
+          return Center(
+            child: Container(
+              width: 360.0,
+              child: Card(
+                margin: EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
                       padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        children: [
-                          TimelineTile(
-                            nodeAlign: TimelineNodeAlign.start,
-                            node: TimelineNode(
-                              indicator: OutlinedDotIndicator(
-                                borderWidth: 2.5,
-                              ),
-                              endConnector: SolidLineConnector(),
-                            ),
-                            contents: Padding(
-                              padding: const EdgeInsets.only(left: 8.0),
-                              child: _InnerTimeline(
-                                title: 'Package Process',
-                                messages: [
-                                  '8:30am Package received by driver',
-                                  '11:30am Reached halfway mark',
-                                ],
-                              ),
-                            ),
-                          ),
-                          TimelineTile(
-                            nodeAlign: TimelineNodeAlign.start,
-                            node: TimelineNode(
-                              indicator: OutlinedDotIndicator(
-                                borderWidth: 2.5,
-                              ),
-                              endConnector: SolidLineConnector(color: Color(0xff66c97f)),
-                            ),
-                            contents: Padding(
-                              padding: const EdgeInsets.only(left: 8.0),
-                              child: _InnerTimeline(
-                                title: 'In Transit',
-                                messages: [
-                                  '13:00pm Driver arrived at destination',
-                                  '11:35am Package delivered by m.vassiliades',
-                                ],
-                              ),
-                            ),
-                          ),
-                          TimelineTile(
-                            nodeAlign: TimelineNodeAlign.start,
-                            node: TimelineNode(
-                              indicator: DotIndicator(
-                                color: Color(0xff66c97f),
-                                child: Icon(
-                                  Icons.check,
-                                  color: Colors.white,
-                                  size: 12.0,
-                                ),
-                              ),
-                              endConnector: SolidLineConnector(),
-                            ),
-                          ),
-                        ],
+                      child: _OrderTitle(
+                        orderInfo: data,
                       ),
                     ),
-                  ),
-                  Divider(height: 1.0),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Row(
-                      children: [
-                        Builder(
-                          builder: (context) => MaterialButton(
-                            onPressed: () {
-                              Scaffold.of(context).showSnackBar(
-                                SnackBar(
-                                  content: Text('On-time!'),
-                                ),
-                              );
-                            },
-                            elevation: 0,
-                            shape: StadiumBorder(),
-                            color: Color(0xff66c97f),
-                            textColor: Colors.white,
-                            child: Text('On-time'),
-                          ),
-                        ),
-                        Spacer(),
-                        Text(
-                          'Driver\nPhilipe',
-                          textAlign: TextAlign.center,
-                        ),
-                        SizedBox(width: 12.0),
-                        Container(
-                          width: 40.0,
-                          height: 40.0,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            image: DecorationImage(
-                              fit: BoxFit.fitWidth,
-                              image: NetworkImage(
-                                'https://i.pinimg.com/originals/08/45/81/084581e3155d339376bf1d0e17979dc6.jpg',
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
+                    Divider(height: 1.0),
+                    _DeliveryProcesses(processes: data.deliveryProcesses),
+                    Divider(height: 1.0),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: _OnTimeBar(driver: data.driverInfo),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _OrderTitle extends StatelessWidget {
+  const _OrderTitle({
+    Key key,
+    @required this.orderInfo,
+  })  : assert(orderInfo != null),
+        super(key: key);
+
+  final _OrderInfo orderInfo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          'Delivery #${orderInfo.id}',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Spacer(),
+        Text(
+          '${orderInfo.date.day}/${orderInfo.date.month}/${orderInfo.date.year}',
+          style: TextStyle(
+            color: Color(0xffb6b2b2),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _InnerTimeline extends StatelessWidget {
+  const _InnerTimeline({
+    @required this.messages,
+  });
+
+  final List<_DeliveryMessage> messages;
+
+  @override
+  Widget build(BuildContext context) {
+    bool isEdgeIndex(int index) {
+      return index == 0 || index == messages.length + 1;
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: FixedTimeline.tileBuilder(
+        theme: TimelineTheme.of(context).copyWith(
+          nodePosition: 0,
+          connectorTheme: TimelineTheme.of(context).connectorTheme.copyWith(
+                thickness: 1.0,
+              ),
+          indicatorTheme: TimelineTheme.of(context).indicatorTheme.copyWith(
+                size: 10.0,
+                position: 0.5,
+              ),
+        ),
+        builder: TimelineTileBuilder(
+          indicatorBuilder: (_, index) => !isEdgeIndex(index) ? Indicator.outlined(borderWidth: 1.0) : null,
+          startConnectorBuilder: (_, index) => Connector.solidLine(),
+          endConnectorBuilder: (_, index) => Connector.solidLine(),
+          contentsBuilder: (_, index) {
+            if (isEdgeIndex(index)) {
+              return null;
+            }
+
+            return Padding(
+              padding: EdgeInsets.only(left: 8.0),
+              child: Text(messages[index - 1].toString()),
+            );
+          },
+          itemExtentBuilder: (_, index) => isEdgeIndex(index) ? 10.0 : 30.0,
+          nodeItemOverlapBuilder: (_, index) => isEdgeIndex(index) ? true : null,
+          itemCount: messages.length + 2,
+        ),
+      ),
+    );
+  }
+}
+
+class _DeliveryProcesses extends StatelessWidget {
+  const _DeliveryProcesses({Key key, @required this.processes})
+      : assert(processes != null),
+        super(key: key);
+
+  final List<_DeliveryProcess> processes;
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTextStyle(
+      style: TextStyle(
+        color: Color(0xff9b9b9b),
+        fontSize: 12.5,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: FixedTimeline.tileBuilder(
+          theme: TimelineThemeData(
+            nodePosition: 0,
+            color: Color(0xff989898),
+            indicatorTheme: IndicatorThemeData(
+              position: 0,
+              size: 20.0,
+            ),
+            connectorTheme: ConnectorThemeData(
+              thickness: 2.5,
+            ),
+          ),
+          builder: TimelineTileBuilder.connected(
+            connectionDirection: ConnectionDirection.before,
+            itemCount: processes.length,
+            contentsBuilder: (_, index) {
+              if (processes[index].isCompleted) return null;
+
+              return Padding(
+                padding: EdgeInsets.only(left: 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      processes[index].name,
+                      style: DefaultTextStyle.of(context).style.copyWith(
+                            fontSize: 18.0,
+                          ),
+                    ),
+                    _InnerTimeline(messages: processes[index].messages),
+                  ],
+                ),
+              );
+            },
+            indicatorBuilder: (_, index) {
+              if (processes[index].isCompleted) {
+                return DotIndicator(
+                  color: Color(0xff66c97f),
+                  child: Icon(
+                    Icons.check,
+                    color: Colors.white,
+                    size: 12.0,
+                  ),
+                );
+              } else {
+                return OutlinedDotIndicator(
+                  borderWidth: 2.5,
+                );
+              }
+            },
+            connectorBuilder: (_, index, ___) => SolidLineConnector(
+              color: processes[index].isCompleted ? Color(0xff66c97f) : null,
             ),
           ),
         ),
@@ -168,76 +201,129 @@ class PackageDeliveryTrackingPage extends StatelessWidget {
   }
 }
 
-class _InnerTimeline extends StatelessWidget {
-  const _InnerTimeline({
-    @required this.title,
-    @required this.messages,
-  });
+class _OnTimeBar extends StatelessWidget {
+  const _OnTimeBar({Key key, @required this.driver})
+      : assert(driver != null),
+        super(key: key);
 
-  final String title;
-  final List<String> messages;
-
-  Widget _buildMessage(String message) {
-    return Padding(
-      padding: EdgeInsets.only(left: 8.0),
-      child: Text(message),
-    );
-  }
+  final _DriverInfo driver;
 
   @override
   Widget build(BuildContext context) {
-    return TimelineTheme(
-      data: TimelineTheme.of(context).copyWith(
-        connectorTheme: TimelineTheme.of(context).connectorTheme.copyWith(thickness: 1.0),
-        indicatorTheme: TimelineTheme.of(context).indicatorTheme.copyWith(
-              size: 10.0,
-              position: 0.5,
-            ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Builder(
-            builder: (context) => Text(
-              title,
-              style: DefaultTextStyle.of(context).style.copyWith(
-                    fontSize: 18.0,
-                  ),
-            ),
-          ),
-          TimelineTile(
-            nodeAlign: TimelineNodeAlign.start,
-            node: TimelineNode(
-              overlap: true,
-              indicator: Indicator.transparent(),
-              endConnector: SolidLineConnector(),
-            ),
-          ),
-          ...messages.map((message) {
-            return TimelineTile(
-              nodeAlign: TimelineNodeAlign.start,
-              node: TimelineNode(
-                indicator: OutlinedDotIndicator(
-                  borderWidth: 1.0,
-                ),
-                startConnector: SolidLineConnector(),
-                endConnector: SolidLineConnector(),
+    return Row(
+      children: [
+        MaterialButton(
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('On-time!'),
               ),
-              contents: _buildMessage(message),
-              mainAxisExtent: 30.0,
             );
-          }).toList(),
-          TimelineTile(
-            nodeAlign: TimelineNodeAlign.start,
-            node: TimelineNode(
-              overlap: true,
-              indicator: Indicator.transparent(),
-              startConnector: SolidLineConnector(),
+          },
+          elevation: 0,
+          shape: StadiumBorder(),
+          color: Color(0xff66c97f),
+          textColor: Colors.white,
+          child: Text('On-time'),
+        ),
+        Spacer(),
+        Text(
+          'Driver\n${driver.name}',
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(width: 12.0),
+        Container(
+          width: 40.0,
+          height: 40.0,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              fit: BoxFit.fitWidth,
+              image: NetworkImage(
+                driver.thumbnailUrl,
+              ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
+  }
+}
+
+_OrderInfo _data(int id) => _OrderInfo(
+      id: id,
+      date: DateTime.now(),
+      driverInfo: _DriverInfo(
+        name: 'Philipe',
+        thumbnailUrl: 'https://i.pinimg.com/originals/08/45/81/084581e3155d339376bf1d0e17979dc6.jpg',
+      ),
+      deliveryProcesses: [
+        _DeliveryProcess(
+          'Package Process',
+          messages: [
+            _DeliveryMessage('8:30am', 'Package received by driver'),
+            _DeliveryMessage('11:30am', 'Reached halfway mark'),
+          ],
+        ),
+        _DeliveryProcess(
+          'In Transit',
+          messages: [
+            _DeliveryMessage('13:00pm', 'Driver arrived at destination'),
+            _DeliveryMessage('11:35am', 'Package delivered by m.vassiliades'),
+          ],
+        ),
+        _DeliveryProcess.complete(),
+      ],
+    );
+
+class _OrderInfo {
+  const _OrderInfo({
+    @required this.id,
+    @required this.date,
+    @required this.driverInfo,
+    @required this.deliveryProcesses,
+  });
+
+  final int id;
+  final DateTime date;
+  final _DriverInfo driverInfo;
+  final List<_DeliveryProcess> deliveryProcesses;
+}
+
+class _DriverInfo {
+  const _DriverInfo({
+    @required this.name,
+    this.thumbnailUrl,
+  });
+
+  final String name;
+  final String thumbnailUrl;
+}
+
+class _DeliveryProcess {
+  const _DeliveryProcess(
+    this.name, {
+    this.messages = const [],
+  });
+
+  const _DeliveryProcess.complete()
+      : this.name = 'Done',
+        this.messages = const [];
+
+  final String name;
+  final List<_DeliveryMessage> messages;
+
+  bool get isCompleted => name == 'Done';
+}
+
+class _DeliveryMessage {
+  const _DeliveryMessage(this.createdAt, this.message);
+
+  final String createdAt; // final DateTime createdAt;
+  final String message;
+
+  @override
+  String toString() {
+    return '$createdAt $message';
   }
 }
